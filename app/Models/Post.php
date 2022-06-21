24936f7920290961956a441c2c2ba9c45756e31d
K Siraki <kevsiraki@gmail.com>
2022-06-21 14:46:18 -0700

    Initial Commit

---
+++ app/Models/Post.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Concerns\Likes;
+use App\Contracts\Likeable;
+
+class Post extends Model implements Likeable
+{
+    use HasFactory, Likes;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'post_type',
+        'meta_data',
+        'category_id',
+        'author_id',
+    ];
+
+    public function author(){
+        return $this->belongsTo(User::class, 'author_id', 'id');
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function comments(){
+        return $this->hasMany(Comment::class)->with(['author']);
+    }
+
+    public function images(){
+        return $this->hasMany(Image::class);
+    }
+
+    public function videos(){
+        return $this->hasMany(Video::class);
+    }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+}
---
+++ app/Models/User.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Models;
+
+use App\Contracts\Likeable;
+use App\Models\Like;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Builder;
+
+class User extends Authenticatable
+{
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'liked_posts',
+        'disliked_posts',
+        'favourite_posts',
+        'favourite_categories',
+        'preferences',
+        'remember_token',
+    ];
+
+    public function comments(){
+        return $this->hasMany(Comment::class, 'author_id', 'id');
+    }
+
+    public function posts(){
+        return $this->hasMany(Post::class, 'author_id', 'id');
+    }
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'profile_photo_url',
+    ];
+	
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function like(Likeable $likeable): self
+    {
+        if ($this->hasLiked($likeable)) {
+            return $this;
+        }
+
+        (new Like())
+            ->user()->associate($this)
+            ->likeable()->associate($likeable)
+            ->save();
+
+        return $this;
+    }
+
+    public function unlike(Likeable $likeable): self
+    {
+        if (! $this->hasLiked($likeable)) {
+            return $this;
+        }
+
+        $likeable->likes()
+            ->whereHas('user', fn($q) => $q->whereId($this->id))
+            ->delete();
+
+        return $this;
+    }
+
+    public function hasLiked(Likeable $likeable): bool
+    {
+        if (! $likeable->exists) {
+            return false;
+        }
+
+       return $likeable->likes()->whereHas('user', function(Builder $query) {
+return $query->whereId($this->id);
+})->exists();
+    }
+	
+}
